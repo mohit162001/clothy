2ad095f930d5cb034dbab3d17cd7655319604bd5
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-09 18:55:12 +0530

    context added

--- frontend/src/Pages/RootLayOut.jsx
+++ frontend/src/Pages/RootLayOut.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Navbar from '../Component/Navbar/Navbar'
 import Footer from '../Component/Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import ShopContextProvider from '../Context/ShowContext'
 
 function RootLayOut() {
   return (
     <>
+    <ShopContextProvider>
     <Navbar/>
     <Outlet/>
     <Footer/>
+    </ShopContextProvider>
     </>
   )
 }
