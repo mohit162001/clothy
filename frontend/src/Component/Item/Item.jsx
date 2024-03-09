4b47821c634a6d92044b91ba5eed444d1e425f72
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-09 18:55:00 +0530

    image url updated

--- frontend/src/Component/Item/Item.jsx
+++ frontend/src/Component/Item/Item.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './item.css'
 import { Link } from 'react-router-dom'
 const Item = (props) => {
+
   return (
     <div className="item">
-        <Link to={`/product/${props.id}`} ><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
+        <Link to={`/product/${props.id}`} ><img onClick={window.scrollTo(0,0)} src={"http://localhost:1337"+props.image} alt="" /></Link>
         <p>{props.name}</p>
         <div className="item-prices">
           <div className="item-price-new">
