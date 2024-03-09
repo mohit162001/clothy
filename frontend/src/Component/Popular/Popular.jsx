fe6d1c82cb32886bd7cf9b1ea8b1df59ff4f7a99
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-09 18:56:09 +0530

    fetching popular product data

--- frontend/src/Component/Popular/Popular.jsx
+++ frontend/src/Component/Popular/Popular.jsx
@@ -2,16 +2,29 @@ import React from 'react'
 import './popular.css'
 import data_product from '../Assests/data';
 import Item from '../Item/Item';
+import { useQuery } from '@apollo/client';
+import { GET_POPULAR_PRODUCT } from '../../query/query';
 const Popular = () => {
+  const {data,error,loading} = useQuery(GET_POPULAR_PRODUCT)
   return (
     <div className='popular'>
         <h1>POPULAR IN WOMEN</h1>
         <hr />
-        <div className="popular-item">
-            {data_product.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+
+        {error && <p>Something went wrong</p>}
+        {loading && !error && <p>Loading Popular Products...</p>}
+        {data && !loading && <div className="popular-item">
+            {data.products.data.map((item,i)=>{
+                return <Item
+                key={i}
+                id={item.attributes.id}
+                name={item.attributes.name}
+                image={item.attributes.image.data.attributes.url}
+                new_price={item.attributes.new_price}
+                old_price={item.attributes.old_price}
+              />
             })}
-        </div>
+        </div>}
     </div>
   )
 }
